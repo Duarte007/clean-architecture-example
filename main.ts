d507950ddba7126d9d8ef3684ad709146762e8e2
Matheus Duarte <matheusdcarvalho007@gmail.com>
2023-01-30 20:47:04 -0300

    [lint] add enter

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { ParkingLotRepositoryMySQL } from "./src/infra/repository/ParkingLotRepositoryMySQL";
 
 const parkingLotRepositoryMySQL = new ParkingLotRepositoryMySQL();
+
 (async () => {
   const response = await parkingLotRepositoryMySQL.getParkingLot("shopping");
   console.log(response);
 })();
+
 console.log("main");
